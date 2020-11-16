358addc66199d4762dca7ef27f5138e10c6a938f
Shantan Thota <36533123+tshantan24@users.noreply.github.com>
2020-11-17 02:16:07 +0530

    Latest files

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    # path("email/", views.email, name="email"),
-    # path("contact/", views.contact, name="contact"),
-    path("send/", views.handleMessages, name="handleMessages"),
+    path("email/", views.email, name="email"),
+    path("contact/", views.contact, name="contact"),
+    path("contact/test/", views.handleMessages, name="handleMessages"),
 ]
